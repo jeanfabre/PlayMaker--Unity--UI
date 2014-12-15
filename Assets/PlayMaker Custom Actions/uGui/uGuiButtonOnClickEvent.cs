cacb0cc9f8b93cc9b2d68fa17b41f2f4117c2a69
jeanfabre <contact@fabrejean.net>
2014-12-15 11:13:32 +0300

    Added new action ( based on Lane uGuiButtonEvent, but reworked for bug fixing)

---
+++ Assets/PlayMaker Custom Actions/uGui/uGuiButtonOnClickEvent.cs
@@ -0,0 +1,59 @@
+﻿// (c) Copyright HutongGames, LLC 2010-2014. All rights reserved.
+//--- __ECO__ __ACTION__ ---//
+
+using UnityEngine;
+using uUI = UnityEngine.UI;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory("uGui")]
+	[Tooltip("Fires an event on click.")]
+	public class uGuiButtonOnClickEvent : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(uUI.Button))]
+		[Tooltip("The GameObject with the button ui component.")]
+		public FsmOwnerDefault gameObject;
+
+		[Tooltip("Send this event when Clicked.")]
+		public FsmEvent sendEvent;
+
+		private uUI.Button button;
+		
+		public override void Reset()
+		{
+			gameObject = null;
+			sendEvent = null;
+		}
+		
+		public override void OnEnter()
+		{
+			GameObject go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go!=null)
+			{
+				button = go.GetComponent<uUI.Button>();
+				if (button!=null)
+				{
+					button.onClick.AddListener(DoOnClick);
+				}else{
+					LogError("Missing UI.Button on "+go.name);
+				}
+			}else{
+				LogError("Missing GameObject ");
+			}
+		}
+
+		public override void OnExit()
+		{
+			if (button!=null)
+			{
+				button.onClick.RemoveListener(DoOnClick);
+			}
+		}
+
+		public void DoOnClick()
+		{
+			Fsm.Event(sendEvent);
+		}
+	}
+}
