6048948f66e6e4415e54edf1855ac3560e69878b
djaydino <djay_dino@hotmail.com>
2015-06-29 18:14:58 +0200

    Update uGuiSetSelectedGameObject.cs

--- Assets/PlayMaker Custom Actions/uGui/uGuiSetSelectedGameObject.cs
+++ Assets/PlayMaker Custom Actions/uGui/uGuiSetSelectedGameObject.cs
@@ -1,6 +1,6 @@
 // (c) Copyright HutongGames, LLC 2010-2015. All rights reserved.
 // http://hutonggames.com/playmakerforum/index.php?topic=8452.30
-/*--- __ECO__ __ACTION__ ---*/
+//--- __ECO__ __ACTION__ ---//
 using UnityEngine;
 using UnityEngine.EventSystems;
 
